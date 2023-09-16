920fbd5f13f9a462437c9e3efe3159426ea795b1
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-09-17 00:59:56 +0300

    update_db

--- ip2location_toolkit/downloader/update.py
+++ ip2location_toolkit/downloader/update.py
@@ -1,6 +1,8 @@
 import struct
 import datetime, os, pathlib
-from ..validators import path_validator
+from colorama import Fore
+from ..validators import path_validator, token_validator, db_code_validator
+from .download import download_extract_db
 
 def get_db_header(filepath):
     """
@@ -62,3 +64,47 @@ def new_version_available(filepath):
     if month_over >= 1:
         return True
     return False
+
+def update_db(filepath, db_code, token, force):
+    """
+    Update the IP2Location database file.
+
+    :param filepath: The path to the IP2Location database file.
+    :type filepath: str
+    :param db_code: The code of the database to download.
+    :type db_code: str
+    :param token: Token for authentication.
+    :type token: str
+    :param force: Force update the database file even if the current version is up to date.
+    :type force: bool
+    :return: The path to the updated IP2Location database file.
+    :rtype: str
+    """
+    try:
+        filename = os.path.basename(filepath)
+        dirname = os.path.dirname(filepath)
+
+        print(f"Updating database ({filename})...")
+        filepath = path_validator(filepath)
+
+        if not os.path.isfile(filepath) or not str(filename).upper().endswith('.BIN'):
+            raise ValueError("The path specified is not a valid IP2Location BIN database file")
+
+        if not force:
+            print ("   Checking for new version...")
+            if not new_version_available(filepath):
+                print (f"   Database {Fore.GREEN + filename + Fore.RESET} is up to date.")
+                return
+            print (f"   New version available for {Fore.YELLOW + filename + Fore.RESET}!")
+
+        output_filepath = download_extract_db(db_code, token, dirname)
+        new_filename = os.path.basename(output_filepath)
+        if new_filename != filename:
+            os.rename(output_filepath, filepath)
+            output_filepath = filepath
+
+        return output_filepath
+    except ValueError as e:
+        print(f'Failed to update {Fore.RED + filename + Fore.RESET}: {str(e)}')
+
+update_db("C:/users/BigBoss/Desktop\IP2LOCATION-LITE-DB1.BIN", "DB1LITEBIN", "r3NQ3DbpDUPC7uRkSLmlI2JHvX8TH5zO5OwEpOAojSAmJnBW8q6srF4uIpVaddEb", True)
